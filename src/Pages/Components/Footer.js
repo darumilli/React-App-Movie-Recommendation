31f79d008b4955d6c72cf622387c3978b005f157
darumilli <152580220+darumilli@users.noreply.github.com>
2024-12-12 23:39:47 -0500

    Add Movie Recommendation React App

---
+++ src/Pages/Components/Footer.js
@@ -0,0 +1,39 @@
+// Footer Component
+import "./styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <div id="row1" className="row ">
+                    <a
+                        href="https://www.linkedin.com/in/divya-sri-arumilli-51938b296/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-linkedin"></i>
+                    </a>
+                    <a
+                        href="https://github.com/darumilli"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a
+                        href="https://www.instagram.com/divya_arumilli/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i className="fa fa-instagram"></i>
+                    </a>
+                </div>
+                <div id="row1" className="row">
+                    © Developed By Divya Arumilli
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
